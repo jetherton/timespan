42ad22f21623fd81263dfe7d023f278613494396
Pablo Destefanis <pdestefanis@gmail.com>
2011-12-02 14:55:43 -0500

    Added Spanish translation file

---
+++ i18n/es_AR/timespan.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *  Universal Transverse Mercator(UTM) English Language file
+ *
+ * This plugin was written for Ushahidi Liberia, by the contractors of Ushahidi Liberia
+ * 2011
+ *
+ * @package  UTM plugin
+ * @author     Carter Draper <carjimdra@gmail.com>
+ * 
+ */
+
+$lang = array(
+'timespan' => 'Configuración de Rango Temporal',
+'timespan_description' => 'Los rangos temporales se redondean al primero del mes siguiente.
+					  <br/>Si se selecciona 24/3/2009, esto se redondeará a 1/4/2009<br/>
+						tanto en la línea de tiempo como en el mapa.',
+'interval' => 'Tipo de intervalo (días/meses)?',
+'calculate' => 'Forma de calcular el intervalo por omisión',
+'days' => 'Número de días en el pasado:',
+'back' => 'Cantidad de días en el pasado, desde la fecha actual',
+'dates' => 'Utilizando fecha inicial y final:',
+'start' => 'Fecha Inicial:',
+'end' => 'Fecha Final:',
+'back' => 'Mostrar todos desde la fecha actual hasta x días atrás',
+'date' => 'Mostrar todos en un rango de fechas',
+'show' => 'Mostrar todos',
+'active' => 'Mostrar el mes más activo',
+'month' => 'Meses',
+'day' => 'Días',
+
+);
